9c41d7a37e9fec56bc60af2aebb1ea03d90f7aa5
michaelb <michaelpb@gmail.com>
2017-08-25 15:09:42 -0700

    adding e2e tests for start

---
+++ spec/endtoend/start-page.js
@@ -0,0 +1,31 @@
+const { spectronLaunch, waitUntilMounted, strip } = require('elmoed').testutils;
+
+describe('Start screen e2e', () => {
+    const ARGS = ['lib/main.js', '--show-start'];
+    let app = null;
+    let text = null;
+    beforeEach((done) => {
+        spectronLaunch(...ARGS, (application) => {
+            app = application;
+            waitUntilMounted(app, () => {
+                app.client.getText('body').then((bodyText) => {
+                    text = bodyText;
+                    done();
+                });
+            });
+        });
+    });
+
+    it('shows a start menu', (done) => {
+        const EXPECTED_START = strip(`
+            Whiteboard
+            Open deck...
+            New deck...
+            Import from directory...
+            About
+        `).toLowerCase();
+        expect(strip(text).toLowerCase()).toContain(EXPECTED_START);
+        done();
+    });
+});
+
