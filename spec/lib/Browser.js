73e5d052365349e234cce12882fe68cf67b63404
michaelb <michaelpb@gmail.com>
2017-08-31 20:40:15 -0700

    adding test for Browser

---
+++ spec/lib/Browser.js
@@ -0,0 +1,41 @@
+/*
+Very basic smoke test for browser module
+*/
+
+/* eslint-disable no-param-reassign */
+
+const { mockWindowManager, strip } = require('elmoed').testutils;
+const Browser = require('../../lib/browser/Browser');
+const path = require('path');
+
+// Integrat-y unit tests for Browser viewer
+describe('Browser', () => {
+    let manager = null;
+
+    beforeEach(() => {
+        ({ manager } = mockWindowManager('browser', Browser));
+    });
+
+    afterEach(() => {
+        manager = null;
+    });
+
+    it('successfully loads a blank URL', (done) => {
+        manager.createWindow('browser', (browser) => {
+            expect(Object.keys(browser.getProps())).toEqual(['url']);
+            const {url} = browser.getProps();
+            expect(url).toEqual('');
+            done();
+        }, '');
+    });
+
+    it('successfully loads a populated URL', (done) => {
+        manager.createWindow('browser', (browser) => {
+            expect(Object.keys(browser.getProps())).toEqual(['url']);
+            const {url} = browser.getProps();
+            expect(url).toEqual('http://lol.com/');
+            done();
+        }, 'http://lol.com/');
+    });
+
+});
