3da5a9d30aec6ccbd3620b44c60f831ed1a72386
michaelb <michaelpb@gmail.com>
2017-09-17 22:23:51 -0700

    switching to custom doc generation refs #46

---
+++ docs/buildsite.js
@@ -0,0 +1,31 @@
+/*
+Very simple static site generator thrown together in a few minutes to replace
+an even simpler cat-based one in the Makefile
+*/
+const fs = require('fs');
+const path = require('path');
+const globSync = require('glob').sync;
+const TinyTiny = require('tinytiny');
+const context = require('./src/context.json');
+context.packageJson = require('../package.json');
+
+for (const filename of globSync(path.resolve(__dirname, 'src', 'includes', '*.html'))) {
+    const bname = path.basename(filename);
+    const contents = fs.readFileSync(filename).toString();
+    const template = TinyTiny(contents);
+    const result = template(context);
+    context['includes_' + bname.split('.')[0]] = result;
+}
+
+for (const filename of globSync(path.resolve(__dirname, 'src', '*.html'))) {
+    const contents = fs.readFileSync(filename).toString();
+    const template = TinyTiny(contents);
+    const result = template(context);
+    const bname = path.basename(filename);
+    const outputPath = path.resolve(__dirname, bname);
+    // fs.writeFileSync(outputPath, result);
+    console.log('result', outputPath);
+}
+
+console.log('result', context);
+
